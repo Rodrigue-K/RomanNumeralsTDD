0c54b8803ae3931e809329cb5e32a3da40e43147
Simon Cass <scass1991@gmail.com>
2019-02-10 17:51:01 +0000

    Add failing test for 2

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -4,4 +4,8 @@ describe RomanNumerals do
   it "Should convert 1 to I" do
     expect(RomanNumerals.romanise(1)).to eq("I")
   end
+
+  it "Should convert 2 to II" do
+    expect(RomanNumerals.romanise(2)).to eq("II")
+  end
 end
