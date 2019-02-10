8d5b5b1074401f4aee1a29e7677aab437e66e558
Simon Cass <scass1991@gmail.com>
2019-02-10 18:01:38 +0000

    Added functionality for multiples of 5

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -4,7 +4,8 @@ describe RomanNumerals do
   {
     1 => "I",
     2 => "II",
-    3 => "III"
+    3 => "III",
+    5 => "V"
   }.each do |arabic, roman|
     it "Should convert #{arabic} to #{roman}" do
       expect(RomanNumerals.romanise(arabic)).to eq(roman)
--- src/roman_numerals.rb
+++ src/roman_numerals.rb
@@ -2,11 +2,15 @@ class RomanNumerals
 
   def self.romanise(number)
     converted = ""
+    if number >= 5
+      converted << "V"
+      number -= 5
+    end
+
     while number >= 1
       converted << "I"
       number -= 1
     end
     converted
   end
-
 end
