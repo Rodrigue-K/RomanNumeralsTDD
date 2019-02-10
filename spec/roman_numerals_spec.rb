4bebf76454c858b2753c3b10075de4a07a99a129
Simon Cass <scass1991@gmail.com>
2019-02-10 18:21:46 +0000

    Linting added, challenge completed

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,14 +1,21 @@
-require "./src/roman_numerals"
+require './src/roman_numerals'
 
 describe RomanNumerals do
   {
-    1 => "I",
-    2 => "II",
-    3 => "III",
-    5 => "V",
-    6 => "VI",
-    9 => "IX",
-    10 => "X"
+    1 => 'I',
+    2 => 'II',
+    3 => 'III',
+    4 => 'IV',
+    5 => 'V',
+    6 => 'VI',
+    9 => 'IX',
+    10 => 'X',
+    14 => 'XIV',
+    19 => 'XIX',
+    50 => 'L',
+    100 => 'M',
+    500 => 'C',
+    1000 => 'D'
   }.each do |arabic, roman|
     it "Should convert #{arabic} to #{roman}" do
       expect(RomanNumerals.romanise(arabic)).to eq(roman)
--- src/roman_numerals.rb
+++ src/roman_numerals.rb
@@ -1,17 +1,15 @@
+# rubocop is weird
 class RomanNumerals
-
   def self.romanise(number)
-    converted = ""
-    {
-      10 => "X",
-      5 => "V",
-      1 => "I"
-    }.each do |arabic, roman|
-    while number >= arabic
-      converted << roman
-      number -= arabic
+    converted = ''
+    { 1000 => 'D', 500 => 'C', 100 => 'M', 50 => 'L', 19 => 'XIX',
+      14 => 'XIV', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV',
+      1 => 'I' }.each do |arabic, roman|
+      while number >= arabic
+        converted << roman
+        number -= arabic
+      end
     end
+    converted
   end
-  converted
-end
 end
